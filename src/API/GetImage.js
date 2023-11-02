0ebe905685c19914b210183d24f18f43a52775ef
asmi-g <asmi.gujral@gmail.com>
2023-11-01 21:55:27 -0400

    Changes

--- src/API/GetImage.js
+++ src/API/GetImage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const apiKey = "API KEY";
+const apiKey = "Uf9d5B4qejVvKyz06egbENSo0TTyfeDjDEQIlpH2";
 const url = "https://api.nasa.gov/planetary/apod?";
 
 let container = document.querySelector(".container");
